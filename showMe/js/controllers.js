9081efb39d7f4eb3767f796414a3434ad97d65c6
wangxinglong <hacke2@qq.com>
2014-08-27 23:28:11 +0800

    加入showMe页面
    Signed-off-by: wangxinglong <hacke2@qq.com>

---
+++ showMe/js/controllers.js
@@ -0,0 +1,147 @@
+
+
+function Kewei($scope) {
+
+  $scope.project = {
+  	name : '科技奖励',
+  	address : 'www.cqnetwork-sp.com:8070',
+  	introduction : '科委工作人员从大量的事务性、重复性工作中解放出来。将科技成果的申报、审核、评审、合同、管理、咨询、统计、信用等事务性过程，纳入信息化管理以提高工作效率',
+  	details : [
+  		{
+  			img : "../img/kewei_2.jpg",
+  			description : "仿WORD在线填写奖励申报书"
+  		},
+  		{
+  			img : "../img/kewei_1.jpg",
+  			description : "通过奖励批次查看当前申请奖励的申报书、单位申报历史，执行撤回，通过等功能"
+  		}
+  	]
+  }
+}
+
+
+function Gaotan($scope) {
+
+  $scope.project = {
+  	name : '高滩川渝合作示范园',
+  	introduction : '四川省广安邻水高滩川渝合作示范园宣传门户网站',
+  	address : 'www.gtcyhzsfy.com',
+  	details : [
+  		{
+  			img : "../img/linshui_2.jpg",
+  			description : "高滩川渝合作示范园首页，使用图片压缩技术、数据缓存技术"
+  		},
+  		{
+  			img : "../img/linshui_1.jpg",
+  			description : "高滩川渝合作示范园园区动态，使用数据缓存技术展示新闻标题"
+  		}
+  	]
+  }
+}
+
+function Guanli($scope) {
+
+  $scope.project = {
+  	name : '重庆理工大学管理学院门户网站',
+  	introduction : '重庆理工大学管理学院门户网站门户网站，负责通知、新闻的发布，涵盖校友通讯录、留言板等附属功能',
+  	address : 'gl.cqut.edu.cn',
+  	details : [
+  		{
+  			img : "../img/glxy_1.jpg",
+  			description : "管理学院首页，使用局部页面缓存技术，提高网站访问速度"
+  		},
+  		{
+  			img : "../img/glxy_2.jpg",
+  			description : "管理学院校友名录页面，使用JQGrid显示校友信息"
+  		},
+  		{
+  			img : "../img/glxy_3.jpg",
+  			description : "管理学院学生学习发展与指导中心，是学生活动、学生学习的良好平台"
+  		}
+  	]
+  }
+}
+
+
+function RoadsideAssistance($scope) {
+
+  $scope.project = {
+  	name : '道路救援系统',
+  	introduction : '融入了快捷报案、综合调度、行业决择、财务管理的车辆救援系统。',
+  	address : 'gl.cqut.edu.cn',
+  	details : [
+  		{
+  			img : "../img/dljy_1.png",
+  			description : "新增报案栏目，使用了语音卡服务、百度地图等"
+  		},
+  		{
+  			img : "../img/dljy_2.png",
+  			description : "调度模块，使用调度算法推荐指派最适合的司机"
+  		},
+  		{
+  			img : "../img/dljy_4.png",
+  			description : "道路救援手机端"
+  		},
+      {
+        img : "../img/dljy_3.png",
+        description : "道路救援手机端，实时查看客户、司机位置"
+      }
+  	]
+  }
+}
+
+
+function OSTM($scope) {
+
+  $scope.project = {
+  	name : '重庆理工大学留学生教务系统',
+  	introduction : '重庆理工大学留学生教务的一个管理系统，主要包含培养计划制定，学生选课，教学管理者排课，毕业资格审核，学生、教师信息管理。',
+  	address : '暂未上线',
+  	details : [
+  		{
+  			img : "../img/ostm_1.jpg",
+  			description : "留学生教务系统首页"
+  		},
+  		{
+  			img : "../img/ostm_2.jpg",
+  			description : "留学生教务系统教师排课"
+  		},
+      {
+        img : "../img/ostm_3.jpg",
+        description : "留学生教务系统人员选择器组件"
+      }
+  	]
+  }
+}
+
+
+function Westom($scope) {
+
+  $scope.project = {
+  	name : '智能家居管理系统',
+  	introduction : '智能家居服务器性能监控、智能家居集群管理、智能家居CRM系统。',
+  	address : '218.70.16.198:9090/WestManager',
+  	details : [
+  		{
+  			img : "../img/znjj_1.jpg",
+  			description : "设备监控，使用WEB端控制设备，比如台灯、空调、窗帘等"
+  		},
+  		{
+  			img : "../img/znjj_2.jpg",
+  			description : "实景监控，使用Modeler系统绘制后在WEB端控制手机系统"
+  		},
+  		{
+  			img : "../img/znjj_3.jpg",
+  			description : "Modeler图形绘制系统，在线绘制用户界面生成XML供用户手机端来解析"
+  		},
+      {
+        img : "../img/znjj_4.jpg",
+        description : "报警监控报表"
+      },
+      {
+        img : "../img/znjj_5.jpg",
+        description : "CRM系统客户地区分布"
+      }
+  	]
+  }
+}
