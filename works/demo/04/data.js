d1b585497a4ce70295e6b95e82d014beba8b7f81
wangxinglong <hacke2@qq.com>
2014-09-12 18:10:01 +0800

    联想招聘

---
+++ works/demo/04/data.js
@@ -0,0 +1,81 @@
+var data = [];
+
+data.push({
+	'name' : 'Dillon',
+	'enname' : 'Dillon',
+	'desc' : '来自哈佛的战略伙伴经理',
+	'pic' : 'images/n8_10.png',
+	'bigpic' : 'images/j2.jpg'
+});
+
+data.push({
+	'name' : '成俞晟',
+	'enname' : 'cheng',
+	'desc' : '技术宅服务器工程师',
+	'pic' : 'images/n8_07.png',
+	'bigpic' : 'images/j3.jpg'
+});
+
+data.push({
+	'name' : 'Said',
+	'enname' : 'Said',
+	'desc' : '开朗健谈的中国通',
+	'pic' : 'images/n8_18.png',
+	'bigpic' : 'images/j5.jpg'
+});
+
+data.push({
+	'name' : '段路路',
+	'enname' : 'duan lulu',
+	'desc' : '爱生活的百变游戏MM',
+	'pic' : 'images/n8_13.png',
+	'bigpic' : 'images/j4.jpg'
+});
+
+data.push({
+	'name' : '宋琦',
+	'enname' : 'song qi',
+	'desc' : '小清新专利保护女超人',
+	'pic' : 'images/n8_15.png',
+	'bigpic' : 'images/j1.jpg'
+});
+
+data.push({
+	'name' : '孙小清',
+	'enname' : 'song xiao qing',
+	'desc' : '懂管理的大客户销售',
+	'pic' : 'images/n8_25.png',
+	'bigpic' : 'images/j6.jpg'
+});
+
+data.push({
+	'name' : '孙玉莎',
+	'enname' : 'song yu sha ',
+	'desc' : '联想给了我更大的舞台',
+	'pic' : 'images/n8_26.png',
+	'bigpic' : 'images/j7.jpg'
+});
+
+data.push({
+	'name' : '田雨丰',
+	'enname' : 'tian yu feng',
+	'desc' : '开朗又细心的会计暖男',
+	'pic' : 'images/n8_27.png',
+	'bigpic' : 'images/j8.jpg'
+});
+
+data.push({
+	'name' : '许力诺',
+	'enname' : 'xu li nong',
+	'desc' : '不安于现状',
+	'pic' : 'images/n8_28.png',
+	'bigpic' : 'images/j10.jpg'
+});
+
+data.push({
+	'name' : '于乐',
+	'enname' : 'yu le',
+	'desc' : '与联想同龄的研发工程师',
+	'pic' : 'images/n8_29.png',
+	'bigpic' : 'images/j11.jpg'
+});
