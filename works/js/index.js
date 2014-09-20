f5b5de3c3e8ae701c1c5b6b85d24aad4ad8a681b
wangxinglong <hacke2@qq.com>
2014-09-20 15:15:26 +0800

    模板测试

--- works/js/index.js
+++ works/js/index.js
@@ -9,31 +9,31 @@ showApp.config(function($routeProvider) {
             templateUrl : 'mian.html'
         })
         .when('/hot-site', {
-            templateUrl : 'template/show.html'
+            templateUrl : 'template/show.html',
+            controller  : 'showController'
         })
         .when('/westom', {
             templateUrl : 'pages/project.html',
             controller  : 'westomController'
         });
 });
-showApp.controller('mainController', function($scope) {
-    $scope.message = {
-    	name : '王兴龙',
-    	mail :　'hacke2@qq.com',
-    	other : '以下填写个人介绍'
-    };
+showApp.controller('showController', function($scope) {
+    $scope.works = [
+    	{
+    		name : 'CSS3弹窗&切换',
+    		desc : '模仿联想招聘',
+    		url : '/works/demo/04/'
+    	},
+    	{
+    		name : '无缝切换图片',
+    		desc : '模仿百度爱玩',
+    		url : '/demo/javascript-seamless-handover/slide/bd01.html'
+    	},
+    	{
+    		name : '榜单tab切换',
+    		desc : '模仿百度爱玩',
+    		url : '/works/demo/03/bd03.html'
+    	}
+    ];
 });
  
-showApp.controller('roadsideAssistanceController', function($scope) {
-    $scope.project = {
-    	name : '道路救援',
-    	other : '以下为其他信息'
-    };
-});
- 
-showApp.controller('westomController', function($scope) {
-    $scope.project = {
-    	name : '威士顿智能家居',
-    	other : '以下为其他信息'
-    };
-});
