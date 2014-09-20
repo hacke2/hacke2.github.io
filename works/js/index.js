22afc28993996d350f73607092347a13ffc03016
wangxinglong <hacke2@qq.com>
2014-09-20 14:58:58 +0800

    第三版展示页面

---
+++ works/js/index.js
@@ -0,0 +1,40 @@
+
+//将路由注入进来
+var showApp= angular.module('project-show', ['ngRoute']);
+ 
+//配置路由
+showApp.config(function($routeProvider) {
+    $routeProvider
+	.when('/', {
+            templateUrl : 'template/show.html'
+        })
+        .when('/roadsideAssistance', {
+            templateUrl : 'pages/project.html',
+            controller  : 'roadsideAssistanceController'
+        })
+        .when('/westom', {
+            templateUrl : 'pages/project.html',
+            controller  : 'westomController'
+        });
+});
+showApp.controller('mainController', function($scope) {
+    $scope.message = {
+    	name : '王兴龙',
+    	mail :　'hacke2@qq.com',
+    	other : '以下填写个人介绍'
+    };
+});
+ 
+showApp.controller('roadsideAssistanceController', function($scope) {
+    $scope.project = {
+    	name : '道路救援',
+    	other : '以下为其他信息'
+    };
+});
+ 
+showApp.controller('westomController', function($scope) {
+    $scope.project = {
+    	name : '威士顿智能家居',
+    	other : '以下为其他信息'
+    };
+});
