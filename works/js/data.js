96ef774b7cf0bdf3a602caa30ba2340e709d1dfb
wangxinglong <hacke2@qq.com>
2014-09-24 00:06:28 +0800

    将数据抽离出来
    Signed-off-by: wangxinglong <hacke2@qq.com>

---
+++ works/js/data.js
@@ -0,0 +1,61 @@
+var data = {
+	hotSite : [
+	    	{
+	    		name : '榜单tab切换',
+	    		desc : '模仿百度爱玩',
+	    		url : '/works/demo/03/bd03.html'
+	    	},
+	    	{
+	    		name : 'CSS3弹窗&切换',
+	    		desc : '模仿联想招聘',
+	    		url : '/works/demo/04/'
+	    	},
+	    	{
+	    		name : '无缝切换图片',
+	    		desc : '模仿百度爱玩',
+	    		url : '/demo/javascript-seamless-handover/slide/bd01.html'
+	    	},
+	    	{
+	    		name : '电商菜单导航',
+	    		desc : '百度微购',
+	    		url : '/works/demo/08/'
+	    	},
+	    	{
+	    		name : '查看长图',
+	    		desc : '模仿QQ空间',
+	    		url : '/works/demo/qqzone-img'
+	    	},
+	    	{
+	    		name : '中秋送好礼',
+	    		desc : '百度微购图片轮播',
+	    		url : '/works/demo/07/'
+	    	},
+	    	{
+	    		name : '层次图片轮播',
+	    		desc : '模仿百度爱玩',
+	    		url : '/works/demo/06/'
+	    	}
+    	],
+    javascript : [
+	    	{
+	    		name : '瀑布流-浮动',
+	    		desc : '瀑布流-浮动',
+	    		url : '/works/demo/stream/float.html'
+	    	},
+	    	{
+	    		name : '瀑布流-定位',
+	    		desc : '瀑布流-定位',
+	    		url : '/works/demo/stream/position.html'
+	    	}
+	    	
+	    ],
+	   
+	html5 : [
+	    	{
+	    		name : '阿里巴巴响应式笔试题',
+	    		desc : '响应式DEMO',
+	    		url : '/works/demo/html5/ali.html'
+	    	}
+	    ]
+    
+}
--- works/js/index.js
+++ works/js/index.js
@@ -13,7 +13,7 @@ showApp.config(function($routeProvider) {
             templateUrl : 'template/show.html',
             controller  : 'hotSiteController'
         })
-        .when('/html5', {redirectTo: '/'})
+        .when('/html5', {redirectTo: '/html5/0'})
         .when('/html5/:index', {
             templateUrl : 'template/show.html',
             controller  : 'html5Controller'
@@ -35,43 +35,7 @@ showApp.controller('hotSiteController', function($scope, $routeParams) {
 	
 	$scope.routeName = 'hot-site';
 	
-    $scope.works = [
-    	{
-    		name : '榜单tab切换',
-    		desc : '模仿百度爱玩',
-    		url : '/works/demo/03/bd03.html'
-    	},
-    	{
-    		name : 'CSS3弹窗&切换',
-    		desc : '模仿联想招聘',
-    		url : '/works/demo/04/'
-    	},
-    	{
-    		name : '无缝切换图片',
-    		desc : '模仿百度爱玩',
-    		url : '/demo/javascript-seamless-handover/slide/bd01.html'
-    	},
-    	{
-    		name : '电商菜单导航',
-    		desc : '百度微购',
-    		url : '/works/demo/08/'
-    	},
-    	{
-    		name : '查看长图',
-    		desc : '模仿QQ空间',
-    		url : '/works/demo/qqzone-img'
-    	},
-    	{
-    		name : '中秋送好礼',
-    		desc : '百度微购图片轮播',
-    		url : '/works/demo/07/'
-    	},
-    	{
-    		name : '层次图片轮播',
-    		desc : '模仿百度爱玩',
-    		url : '/works/demo/06/'
-    	}
-    ];
+    $scope.works = data.hotSite;
     
     document.getElementById('mainIframe').setAttribute('src', $scope.works[index].url);
 });
@@ -82,19 +46,18 @@ showApp.controller('javascriptController', function($scope, $routeParams) {
 	
 	$scope.routeName = 'javascript';
 	
-    $scope.works = [
-    	{
-    		name : '瀑布流-浮动',
-    		desc : '瀑布流-浮动',
-    		url : '/works/demo/stream/float.html'
-    	},
-    	{
-    		name : '瀑布流-定位',
-    		desc : '瀑布流-定位',
-    		url : '/works/demo/stream/position.html'
-    	}
-    	
-    ];
+    $scope.works = data.javascript;
+    
+    document.getElementById('mainIframe').setAttribute('src', $scope.works[index].url);
+});
+
+showApp.controller('html5Controller', function($scope, $routeParams) {
+	
+	var index = $routeParams.index;
+	
+	$scope.routeName = 'html5';
+	
+    $scope.works = data.html5;
     
     document.getElementById('mainIframe').setAttribute('src', $scope.works[index].url);
 });
